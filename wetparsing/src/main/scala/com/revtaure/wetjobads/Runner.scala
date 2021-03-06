8e4f2c0b9dc509c71f08bbbf93a699358ce99c18
hayden7913 <hayden321@gmail.com>
2021-03-05 17:57:16 -0800

    prints job ad contents

--- wetparsing/src/main/scala/com/revtaure/wetjobads/Runner.scala
+++ wetparsing/src/main/scala/com/revtaure/wetjobads/Runner.scala
@@ -21,6 +21,8 @@ object Runner {
     import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
 
     val file = "CC-MAIN-20131204131715-00000-ip-10-33-133-15.ec2.internal.warc.wet"
+    // val file = "/Users/hayden/temp/CC-MAIN-20200229114448-20200229144448-00551.warc.wet.gz"
+    // val file = "head.wet"
     val conf = new Configuration(sc.hadoopConfiguration)
     conf.set("textinputformat.record.delimiter", "WARC/1.0")
     val input = sc.newAPIHadoopFile(
@@ -31,8 +33,28 @@ object Runner {
       conf
     )
 
-    val lines = input.map { case (_, text) => text.toString }
-    lines.take(200).foreach(println)
+    val records = input.map { case (_, text) => text.toString }
 
+    records
+      .filter(record => {
+        val lines = record.split("\n")
+        val uriContainsJob = lines
+          .find(_.startsWith("WARC-Target-URI:"))
+          .map(uriHeader => uriHeader.split(" "))
+          .map(split => if (split.length == 2) split(1) else "")
+          .map(uri => uri.contains("job"))
+
+        uriContainsJob.getOrElse(false)
+      })
+      .map(record => {
+        val lines = record.split("\n")
+        val withoutHeaders = lines.filter(l => {
+          (!l.startsWith("WARC") || l.startsWith("WARC-Target-URI:")) &&
+          !l.startsWith("Content-Type:") &&
+          !l.startsWith("Content-Length:") 
+        })
+        withoutHeaders.mkString("\n")
+      })
+      .foreach(println)
   }
 }
