1d04e495b2695f66352e66b0e3aec0a352029bda
Jefferson Santiago <jeffersonrsantiago86@gmail.com>
2021-03-08 13:50:02 -0800

    Sooo many issues :(

--- project3/scala_s3_read-main/scalas3read/src/main/scala/com/revature/scalas3read/Runner.scala
+++ project3/scala_s3_read-main/scalas3read/src/main/scala/com/revature/scalas3read/Runner.scala
@@ -16,17 +16,14 @@ object Runner {
     val spark = SparkSession
       .builder()
       .appName("scalas3read")
-      //.master("local[*]")
+      .master("local[*]")
       .getOrCreate()
 
     
 
     // Reference: https://sparkbyexamples.com/spark/spark-read-text-file-from-s3/#s3-dependency
-    //val key = System.getenv("("AWS_KEY")")
-    //val secret = System.getenv(("AWS_SECRET_KEY"))
-
-    val key = "AKIAJCL42GFQFYIBPBKA"
-    val secret = ""
+    val key = System.getenv(("AWS_KEY"))
+    val secret = System.getenv(("AWS_SECRET_KEY"))
 
     spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", key)
     spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", secret)
@@ -44,9 +41,11 @@ object Runner {
 
     val commonCrawl = spark.read
                       //.option("lineSep", "WARC/1.0")
-                      //CC-MAIN-2021-04/segments/1610704847953.98/wet/CC-MAIN-20210128134124-20210128164124-00799.warc.wet.gz
                       .option("lineSep", "WARC/1.0")
-                      .textFile("s3a://commoncrawl/crawl-data/CC-MAIN-2021-04/segments/1610704843561.95/warc/")
+                      // s3a://commoncrawl/crawl-data/CC-MAIN-2020-34/segments/1596439741154.98/warc/CC-MAIN-20200815184756-20200815214756-00599.warc.gz
+                      //s3a://commoncrawl/crawl-data/CC-MAIN-2021-04/segments/1610704843561.95/warc/CC-MAIN-20210128102756-20210128132756-00738.warc.gz
+                      .textFile("s3a://commoncrawl/crawl-data/CC-MAIN-2020-34/segments/1596439741154.98/warc/CC-MAIN-20200815184756-20200815214756-00599.warc.gz")
+
   
     //commonCrawl.printSchema()
     //commonCrawl.createOrReplaceTempView("data")
@@ -60,16 +59,22 @@ object Runner {
     commonCrawl
       //.select("url_host_name", "url_path")
       //.filter($"crawl" === "CC-MAIN-2021-04")
-      .filter($"value".contains("www.indeed.com") && $"value".contains("tech"))
+      //.filter($"value".contains("www.indeed.com"))
       //.filter($"url_path".contains("career") || $"url_path".contains("software") || $"url_path".contains("developer"))
-      .flatMap(extractWords(_))
+      //.flatMap(extractWords(_))
       //.show(false)
-      .filter($"_2".contains("tech"))
-      .select("_1", "_2")
-      .write
-      .format("csv")
-      .mode("overwrite")
-      .save("s3a://bigdata-pj2-jeff/data/")
+      // .filter($"_2".contains("tech") || 
+      //     $"_2".contains("software") ||  
+      //     $"_2".contains("developer") || 
+      //     $"_2".contains("information technology"))
+      .map(extractWords(_))
+      .show()
+       
+      //  .write
+      //  .mode("overwrite")
+      //  .text("test")
+       
+       //.save("s3a://adam-king-batch-921/scalaemrdata/testresult")
 
     /*
       crawl-data -> Filterm to indeed.com/glassdoor/ w/e job website -> extract the job title, and company
@@ -79,18 +84,20 @@ object Runner {
     */
 
   }
-  //
+  //Map[String, String] 
   def extractWords(line : String) : Map[String, String] = {
     var currLine : String = ""
     currLine = line.split('\n').map(_.trim.filter(_ >= ' ')).mkString
-    currLine = currLine.split("\n\r").map(_.trim.filter(_ >= ' ')).mkString
+    currLine = currLine.split('\r').map(_.trim.filter(_ >= ' ')).mkString
     currLine = currLine.split('\t').map(_.trim.filter(_ >= ' ')).mkString
+
+    //println(currLine)
 
     var mappedTitles : Map[String, String] = Map()
 
-    val regex = """<div class="bti-ui-job-result-detail-employer" style="">([a-zA-Z\- ]+)<\/div>""".r.findAllMatchIn(currLine)
+    val regex = """<div class="bti-ui-job-result-detail-employer" style="">([a-zA-Z\- ]+)<\/div>""".r.anchored.findAllMatchIn(currLine)
 
-    val regex2 = """<a target='_blank' onmousedown="indeed_clk\(this,'9861'\);" href=(["'])(.*?) class="bti-job-detail-link" id="jobURL">([a-zA-Z\- \— \/]+)<\/a>""".r.findAllMatchIn(currLine)
+    val regex2 = """<a target='_blank' onmousedown="indeed_clk\(this,'9861'\);" href=(["'])(.*?) class="bti-job-detail-link" id="jobURL">([a-zA-Z\- \— \/]+)<\/a>""".r.anchored.findAllMatchIn(currLine)
 
     while(regex.hasNext && regex2.hasNext) {
       var company = regex.next().toString.replace("""<div class="bti-ui-job-result-detail-employer" style="">""", "")
@@ -102,7 +109,9 @@ object Runner {
       )
       jobTitle = jobTitle.replace("</a>", "")
 
-      mappedTitles += ( company -> jobTitle)
+      print(regex.next() + " " + regex2.next())
+
+      mappedTitles += ( regex.next().toString ->  regex2.next().toString)
     }
 
     mappedTitles
