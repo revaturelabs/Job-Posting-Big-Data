ce59a5d8cb0058990d5e24020d4e518e68aa0305
hayden7913 <hayden321@gmail.com>
2021-03-09 22:57:25 -0800

    unignore sample_wet_files.py

---
+++ wetjobads/wet_sampling/sample_wet_files.py
@@ -0,0 +1,16 @@
+import os
+import random
+import boto3
+
+s3 = boto3.resource("s3")
+
+SAMPLE_SIZE = 1000
+WET_PATHS = "wet.paths"
+
+with open(WET_PATHS , "r") as f:
+    wet_paths = f.readlines()
+    random_paths = random.sample(wet_paths, SAMPLE_SIZE)
+    paths_string = "".join(random_paths)
+    s3.Object("wet-segments", "random-paths.txt").delete()
+    s3.Object("wet-segments", "random-paths.txt").put(Body=paths_string)
+    
